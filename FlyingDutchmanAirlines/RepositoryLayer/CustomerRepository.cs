e0c98db8f7940891c5d2fe45337fdfb8e2d696d3
Jasper Francisco <jasper.francisco@gmail.com>
2021-04-06 20:19:11 +0800

    Add initial CustomerRepo implem

---
+++ FlyingDutchmanAirlines/RepositoryLayer/CustomerRepository.cs
@@ -0,0 +1,15 @@
+namespace FlyingDutchmanAirlines.RepositoryLayer
+{
+    public class CustomerRepository
+    {
+        public bool CreateCustomer(string name) 
+        { 
+            if (string.IsNullOrEmpty(name))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
