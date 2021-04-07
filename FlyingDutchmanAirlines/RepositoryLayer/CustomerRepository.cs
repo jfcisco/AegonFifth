04294e8329b3b4aa76e01b0ac8884592888e1793
Jasper Francisco <jasper.francisco@gmail.com>
2021-04-07 18:38:12 +0800

    Add capability to save to DB

--- FlyingDutchmanAirlines/RepositoryLayer/CustomerRepository.cs
+++ FlyingDutchmanAirlines/RepositoryLayer/CustomerRepository.cs
@@ -1,15 +1,47 @@
+using System.Linq;
+using System.Threading.Tasks;
+using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.DatabaseLayer.Models;
+
 namespace FlyingDutchmanAirlines.RepositoryLayer
 {
     public class CustomerRepository
     {
-        public bool CreateCustomer(string name) 
+        private readonly FlyingDutchmanAirlinesContext _context;
+        public CustomerRepository(FlyingDutchmanAirlinesContext _context)
+        {
+            this._context = _context;
+        }
+
+        public async Task<bool> CreateCustomer(string name) 
         { 
-            if (string.IsNullOrEmpty(name))
+            if (IsInvalidCustomerName(name))
+            {
+                return false;
+            }
+
+            try
+            {
+                Customer newCustomer = new Customer(name);
+
+                using(_context)
+                {
+                    _context.Customers.Add(newCustomer);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch
             {
                 return false;
             }
 
             return true;
+        }
+
+        private bool IsInvalidCustomerName(string name)
+        {
+            char[] forbiddenCharacters = { '!', '@', '#', '$', '%', '&', '*' };
+            return string.IsNullOrEmpty(name) || name.Any(x => forbiddenCharacters.Contains(x));
         }
     }
 }
