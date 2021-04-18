ef8954809c1f07417d6c6ccc271dcb5e6dd71de1
Jasper Francisco <jasper.francisco@gmail.com>
2021-04-18 18:58:52 +0800

    Add initial unit tests for AirportRepository

--- FlyingDutchmanAirlines_Tests/AIrportRepositoryTests.cs
+++ FlyingDutchmanAirlines_Tests/AIrportRepositoryTests.cs
@@ -1,5 +1,9 @@
+using System.Threading.Tasks;
 using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.DatabaseLayer.Models;
+using FlyingDutchmanAirlines.Exceptions;
 using FlyingDutchmanAirlines.RepositoryLayer;
+using FlyingDutchmanAirlines_Tests.Stubs;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -12,15 +16,40 @@ namespace FlyingDutchmanAirlines_Tests.RepositoryLayer
         private AirportRepository _repository;
         
         [TestInitialize]
-        public void TestInitialize()
+        public async Task TestInitialize()
         {
             DbContextOptions<FlyingDutchmanAirlinesContext> dbContextOptions = new DbContextOptionsBuilder<FlyingDutchmanAirlinesContext>().UseInMemoryDatabase("FlyingDutchman").Options;
 
-            _context = new FlyingDutchmanAirlinesContext(dbContextOptions);
+            _context = new FlyingDutchmanAirlinesContext_Stub(dbContextOptions);
+
+            Airport testAirport = new Airport();
+            _context.Airports.Add(testAirport);
+            await _context.SaveChangesAsync();
+
             _repository = new AirportRepository(_context);
 
             Assert.IsNotNull(_repository);
+        }
 
+        [TestMethod]
+        public async Task GetAirportByID_Success()
+        {
+            Airport airport = await _repository.GetAirportByID(1);
+            Assert.IsNotNull(airport);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AirportNotFoundException))]
+        public async Task GetAirportByID_Failure_InvalidArgument()
+        {
+            await _repository.GetAirportByID(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AirportNotFoundException))]
+        public async Task GetAirportByID_Failure_AirportNotExisting()
+        {
+            await _repository.GetAirportByID(2);
         }
     }
     
