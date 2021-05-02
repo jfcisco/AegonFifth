63282d948baea438ac1b75ab68e487cbd3a0d1ad
Jasper Francisco <jasper.francisco@gmail.com>
2021-05-02 10:59:58 +0800

    Add IsPositive extension method

---
+++ FlyingDutchmanAirlines/ExtensionMethods.cs
@@ -0,0 +1,9 @@
+internal static class ExtensionMethods
+{
+
+    // A better naming might be IsNonNegative
+    internal static bool IsPositive(this int input)
+    {
+        return input >= 0;
+    }
+}
