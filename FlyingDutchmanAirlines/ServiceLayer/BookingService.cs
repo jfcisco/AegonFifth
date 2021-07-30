cebd8f8749c18c1bb020e9b571fee209177198bd
Jasper Francisco <jasper.francisco@gmail.com>
2021-07-30 10:45:38 +0800

    Add initial implem and tests for BookingService

---
+++ FlyingDutchmanAirlines/ServiceLayer/BookingService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using FlyingDutchmanAirlines.RepositoryLayer;
+
+namespace FlyingDutchmanAirlines.ServiceLayer
+{
+    public class BookingService
+    {
+        private readonly BookingRepository _bookingRepo;
+        private readonly CustomerRepository _customerRepo;
+        
+        public BookingService(BookingRepository bookingRepo, CustomerRepository customerRepo)
+        {
+            _bookingRepo = bookingRepo;
+            _customerRepo = customerRepo;
+        }
+
+        public async Task<(bool, Exception)> CreateBooking(string customerName, int flightNumber)
+        {
+            return (true, null);
+        }
+    }
+}
---
+++ FlyingDutchmanAirlines_Tests/ServiceLayer/BookingServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.RepositoryLayer;
+using FlyingDutchmanAirlines.ServiceLayer;
+using FlyingDutchmanAirlines_Tests.Stubs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlyingDutchmanAirlines_Tests.ServiceLayer
+{
+    [TestClass]
+    public class BookingServiceTests
+    {
+        private FlyingDutchmanAirlinesContext _context;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            DbContextOptions<FlyingDutchmanAirlinesContext> dbContextOptions =
+                new DbContextOptionsBuilder<FlyingDutchmanAirlinesContext>()
+                    .UseInMemoryDatabase("FlyingDutchman").Options;
+            
+            _context = new FlyingDutchmanAirlinesContext_Stub(dbContextOptions);
+        }
+
+        [TestMethod]
+        public async Task CreateBooking_Success()
+        {
+            BookingRepository repository = new(_context);
+            CustomerRepository customerRepo = new(_context);
+            BookingService service = new(repository, customerRepo);
+            (bool result, Exception exception) = await service.CreateBooking("Leo Tolstoy", 0);
+        }
+    }
+}
