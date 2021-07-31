b71804aa0e92695dd80f7cd9916fedc0badcf4d5
Jasper Francisco <jasper.francisco@gmail.com>
2021-07-31 15:38:22 +0800

    Add error handling for Airport repository

--- FlyingDutchmanAirlines/ServiceLayer/FlightService.cs
+++ FlyingDutchmanAirlines/ServiceLayer/FlightService.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using FlyingDutchmanAirlines.DatabaseLayer.Models;
+using FlyingDutchmanAirlines.Exceptions;
 using FlyingDutchmanAirlines.RepositoryLayer;
 using FlyingDutchmanAirlines.Views;
 
@@ -20,14 +23,34 @@ namespace FlyingDutchmanAirlines.ServiceLayer
         {
             foreach (Flight flight in _flightRepo.GetFlights())
             {
-                Airport origin = await _airportRepo.GetAirportByID(flight.Origin);
-                Airport destination = await _airportRepo.GetAirportByID(flight.Destination);
-                
+                (Airport origin, Airport destination) = await GetAirportsDetails(flight.Origin, flight.Destination);
+
                 yield return new FlightView(
                     flight.FlightNumber.ToString(), 
                     (origin.City, origin.Iata),
                     (destination.City, destination.Iata));
             }
+        }
+
+        private async Task<(Airport origin, Airport destination)> GetAirportsDetails(int originId, int destinationId)
+        {
+            Airport origin;
+            Airport destination;
+            try
+            {
+                origin= await _airportRepo.GetAirportByID(originId);
+                destination = await _airportRepo.GetAirportByID(destinationId);
+            }
+            catch (AirportNotFoundException)
+            {
+                throw new FlightNotFoundException();
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException();
+            }
+
+            return (origin, destination);
         }
     }
 }
--- FlyingDutchmanAirlines_Tests/ServiceLayer/FlightServiceTests.cs
+++ FlyingDutchmanAirlines_Tests/ServiceLayer/FlightServiceTests.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FlyingDutchmanAirlines.DatabaseLayer.Models;
+using FlyingDutchmanAirlines.Exceptions;
 using FlyingDutchmanAirlines.RepositoryLayer;
 using FlyingDutchmanAirlines.ServiceLayer;
 using FlyingDutchmanAirlines.Views;
@@ -20,33 +22,32 @@ namespace FlyingDutchmanAirlines_Tests.ServiceLayer
         {
             _mockFlightRepository = new Mock<FlightRepository>();
             _mockAirportRepository = new Mock<AirportRepository>();
+            
+            Flight savedFlight = new()
+            {
+                FlightNumber = 148,
+                Origin = 31,
+                Destination = 92
+            };
+            
+            Queue<Flight> mockReturn = new(1);
+            mockReturn.Enqueue(savedFlight);
+
+            _mockFlightRepository.Setup(repository => repository.GetFlights()).Returns(mockReturn);
         }
         
         [TestMethod]
         public async Task GetFlights_Success()
         {
-            Flight flightSaved = new()
+            _mockAirportRepository.Setup(repository => repository.GetAirportByID(31)).ReturnsAsync(new Airport()
             {
-                FlightNumber = 1,
-                Origin = 2,
-                Destination = 3
-            };
-
-            Queue<Flight> mockReturn = new Queue<Flight>();
-            mockReturn.Enqueue(flightSaved);
-            
-            _mockFlightRepository.Setup(repository => repository.GetFlights())
-                .Returns(mockReturn);
-
-            _mockAirportRepository.Setup(repository => repository.GetAirportByID(2)).ReturnsAsync(new Airport()
-            {
-                AirportId = 2,
+                AirportId = 31,
                 City = "Mexico City",
                 Iata = "MEX"
             });
-            _mockAirportRepository.Setup(repository => repository.GetAirportByID(3)).ReturnsAsync(new Airport()
+            _mockAirportRepository.Setup(repository => repository.GetAirportByID(92)).ReturnsAsync(new Airport()
             {
-                AirportId = 3,
+                AirportId = 92,
                 City = "Ulaanbaatar",
                 Iata = "UBN"
             });
@@ -56,13 +57,34 @@ namespace FlyingDutchmanAirlines_Tests.ServiceLayer
             await foreach (FlightView flightView in service.GetFlights())
             {
                 Assert.IsNotNull(flightView);
-                Assert.AreEqual("1", flightView.FlightNumber);
+                Assert.AreEqual("148", flightView.FlightNumber);
                 Assert.AreEqual("Mexico City", flightView.Origin.City);
                 Assert.AreEqual("MEX", flightView.Origin.Code);
                 Assert.AreEqual("Ulaanbaatar", flightView.Destination.City);
                 Assert.AreEqual("UBN", flightView.Destination.Code);
             }
         }
-        
+
+        [TestMethod]
+        [ExpectedException(typeof(FlightNotFoundException))]
+        public async Task GetFlights_Failure_FlightNotFound()
+        {
+            _mockAirportRepository.Setup(repository => repository.GetAirportByID(31))
+                .ThrowsAsync(new AirportNotFoundException());
+
+            FlightService service = new(_mockFlightRepository.Object, _mockAirportRepository.Object);
+            await foreach (FlightView _ in service.GetFlights()) {}
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public async Task GetFlights_Failure_GeneralException()
+        {
+            _mockAirportRepository.Setup(repository => repository.GetAirportByID(31))
+                .ThrowsAsync(new NullReferenceException());
+
+            FlightService service = new(_mockFlightRepository.Object, _mockAirportRepository.Object);
+            await foreach (FlightView _ in service.GetFlights()) {}
+        }
     }
 }
