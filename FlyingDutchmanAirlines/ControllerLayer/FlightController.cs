8ce69094743d477ff897b46f82bf8b34494783f5
Jasper Francisco <jasper.francisco@gmail.com>
2021-08-14 19:07:26 +0800

    Resolve unused exception warnings

--- FlyingDutchmanAirlines/ControllerLayer/FlightController.cs
+++ FlyingDutchmanAirlines/ControllerLayer/FlightController.cs
@@ -33,11 +33,11 @@ namespace FlyingDutchmanAirlines.ControllerLayer
 
                 return StatusCode((int) HttpStatusCode.OK, flights);
             }
-            catch (FlightNotFoundException exception)
+            catch (FlightNotFoundException)
             {
                 return StatusCode((int) HttpStatusCode.NotFound, "No flights were found in the database");
             }
-            catch (Exception exception)
+            catch (Exception)
             {
                 return StatusCode((int) HttpStatusCode.InternalServerError, "An error occured");
             }
